d73730ebb7c2b438ec031134dcd5cf6e06c9b4ba
David Jones <djones6@uk.ibm.com>
2018-09-04 16:17:57 +0100

    Update latest to Swift 4.1.2

--- Bench-Kitura-SwiftMetrics/latest/Package.swift
+++ Bench-Kitura-SwiftMetrics/latest/Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Bench-Kitura-SwiftMetrics",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Bench-Kitura-SwiftMetrics",
-            targets: ["HelloWorld", "HelloWorldSwiftMetrics", "HelloWorldSwiftMetricsHTTP"]),
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
--- Bench-Kitura-TechEmpower/latest/Package.swift
+++ Bench-Kitura-TechEmpower/latest/Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Kitura-Bench-TechEmpower",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Kitura-Bench-TechEmpower",
-            targets: ["TechEmpowerCouch", "TechEmpowerKuery", "TechEmpowerKueryPostgres", "TechEmpowerKueryMustache", "TechEmpowerMongoKitten"]),
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -19,8 +13,8 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "3.0.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", from: "2.0.0"),
         .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.8.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", from: "1.7.2"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.10.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", from: "1.8.0"),
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.0"),
     ],
     targets: [
