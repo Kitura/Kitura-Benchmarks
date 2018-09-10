89efef5358be0e27727ebc48f5cf87b92daafb4a
David Jones <djones6@uk.ibm.com>
2018-09-10 13:24:00 +0100

    Handle multiple versions of Xcode for 4.x releases

--- Bench-Swift/lib/install-swift.sh
+++ Bench-Swift/lib/install-swift.sh
@@ -5,6 +5,7 @@ function installSwift {
   # -s flag (skip-existing) prevents failure when build already installed
   local version=`cat .swift-version`
   local swiftMajor=`echo $version | cut -d'.' -f1`
+  local swiftMinor=`echo $version | cut -d'.' -f2`
   case `uname` in
   Linux)
     swiftenv install $version -s
@@ -15,7 +16,9 @@ function installSwift {
 
     # Ensure we select the correct XCode version for this version of swift:
     #   Swift 3.x = XCode 8.3
-    #   Swift 4.x = XCode 9
+    #   Swift 4.0.x = XCode 9.2
+    #   Swift 4.1.x = XCode 9.4
+    #   Swift 4.2.x = XCode 10
     #
     # Note that this relies on having previous versions of XCode installed in a
     # prescribed location (example: /Applications/Xcode8.3.3).
@@ -23,12 +26,26 @@ function installSwift {
     case "$swiftMajor" in
     3)
       echo "Switching to Xcode 8.3.3 for Swift 3.x"
-      sudo xcode-select -s /Applications/Xcode8.3.3.app/Contents/Developer
+      sudo xcode-select -s /Applications/Xcode8.3.3.app
       xcode-select -p
       ;;
     4)
-      echo "Switching to Xcode 9 for Swift 4.x"
-      sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+      case "$swiftMinor" in
+      0)
+        echo "Switching to Xcode 9.2 for Swift 4.0.x"
+        sudo xcode-select -s /Applications/Xcode9.2.app
+        ;;
+      1)
+        echo "Switching to Xcode 9.4 for Swift 4.1.x"
+        sudo xcode-select -s /Applications/Xcode9.4.app
+        ;;
+      2)
+        echo "Switching to Xcode 10.0 for Swift 4.2.x"
+        sudo xcode-select -s /Applications/Xcode10.0.app
+        ;;
+      *)
+        echo "Unknown Swift 4 minor version: ${swiftMinor}"
+      esac
       xcode-select -p
       ;;
     *)
