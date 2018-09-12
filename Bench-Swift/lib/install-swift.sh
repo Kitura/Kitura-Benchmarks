139e4dbbe6f75c258c3c6fc1596a8e13be6a3fda
David Jones <djones6dev@gmail.com>
2018-09-12 13:08:56 +0100

    Handle multiple versions of Xcode for 4.x releases (#4)

--- Bench-Swift/lib/install-swift.sh
+++ Bench-Swift/lib/install-swift.sh
@@ -5,6 +5,7 @@ function installSwift {
   # -s flag (skip-existing) prevents failure when build already installed
   local version=`cat .swift-version`
   local swiftMajor=`echo $version | cut -d'.' -f1`
+  local swiftMinor=`echo $version | cut -d'.' -f2`
   case `uname` in
   Linux)
     swiftenv install $version -s
@@ -15,7 +16,9 @@ function installSwift {
 
     # Ensure we select the correct XCode version for this version of swift:
     #   Swift 3.x = XCode 8.3
-    #   Swift 4.x = XCode 9
+    #   Swift 4.0.x = XCode 9.2
+    #   Swift 4.1.x = XCode 9.4
+    #   Swift 4.2.x = XCode 10
     #
     # Note that this relies on having previous versions of XCode installed in a
     # prescribed location (example: /Applications/Xcode8.3.3).
@@ -23,17 +26,39 @@ function installSwift {
     case "$swiftMajor" in
     3)
       echo "Switching to Xcode 8.3.3 for Swift 3.x"
-      sudo xcode-select -s /Applications/Xcode8.3.3.app/Contents/Developer
-      xcode-select -p
+      switchXcodeIfAvailable /Applications/Xcode8.3.3.app
       ;;
     4)
-      echo "Switching to Xcode 9 for Swift 4.x"
-      sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-      xcode-select -p
+      case "$swiftMinor" in
+      0)
+        echo "Switching to Xcode 9.2 for Swift 4.0.x"
+        switchXcodeIfAvailable /Applications/Xcode9.2.app
+        ;;
+      1)
+        echo "Switching to Xcode 9.4 for Swift 4.1.x"
+        switchXcodeIfAvailable /Applications/Xcode9.4.app
+        ;;
+      2)
+        echo "Switching to Xcode 10.0 for Swift 4.2.x"
+        switchXcodeIfAvailable /Applications/Xcode10.0.app
+        ;;
+      *)
+        echo "Unknown Swift 4 minor version: ${swiftMinor}"
+      esac
       ;;
     *)
       echo "Unknown Swift major version: $swiftMajor"
     esac
+    echo "Selected Xcode installation: `xcode-select -p`"
     ;;
   esac
+}
+
+function switchXcodeIfAvailable {
+  local xcodePath="$1"
+  if [ -d "$xcodePath" ]; then
+    sudo xcode-select -s $xcodePath
+  else
+    echo "No Xcode installation found at $xcodePath"
+  fi
 }
